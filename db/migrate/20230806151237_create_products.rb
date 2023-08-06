3f5fc1d38989cca536e356074bfbe988bda90d7c
moriyama junshi <junshi614@gmail.com>
2023-08-07 00:25:40 +0900

    Implementation on the admin's side of the product

---
+++ db/migrate/20230806151237_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :price, null: false
+      t.integer :stock, null: false, default: 
+      
+      t.timestamps
+    end
+  end
+end
